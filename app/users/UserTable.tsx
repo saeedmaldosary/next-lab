f1e981fc5bc65750b92ff7cac34b2c57c128fb5c
Saeed Aldosary <saldosary@scai.sa>
2024-10-05 12:11:23 +0300

    Implement sorting based on page params

--- app/users/UserTable.tsx
+++ app/users/UserTable.tsx
@@ -1,29 +1,43 @@
 import React from "react";
+import Link from "next/link";
+import { sort } from "fast-sort";
 
 interface User {
   id: number; // Changed from Number to number
   name: string; // Changed from String to string
 }
 
-const UserTable = async () => {
+interface Props {
+  sortOrder: string;
+}
+
+const UserTable = async ({ sortOrder }: Props) => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users", {
     next: { revalidate: 1 }
     // revalidate: next have bulit in cache to refresh the data after period of time we can use revalidate and set the number in seconds or we can use { cache: 'no-store'}
   });
   const users: User[] = await res.json();
+
+  const sortedUsers =
+    sortOrder === "desc"
+      ? sort(users).desc((user) => user.name)
+      : sort(users).asc((user) => user.name);
+
   return (
     <table className="table">
       <thead>
         <tr>
           <th>ID</th>
-          <th>Name</th>
+          <th>
+            <Link href="/users?sortOrder=desc">Name</Link>
+          </th>
         </tr>
       </thead>
       <tbody>
-        {users.map((user) => (
+        {sortedUsers.map((user) => (
           <tr key={user.id}>
-            <td>{user.name}</td>
             <td>{user.id}</td>
+            <td>{user.name}</td>
           </tr>
         ))}
       </tbody>
---
+++ app/users/[[...slug]]/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import UserTable from "../UserTable";
+
+interface Props {
+  searchParams: { sortOrder: string };
+}
+
+const UsersPage = async ({ searchParams: { sortOrder } }: Props) => {
+  return (
+    <div>
+      <h1>Users</h1>
+      <UserTable sortOrder={sortOrder} />
+    </div>
+  );
+};
+
+export default UsersPage;
