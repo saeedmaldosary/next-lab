1530a332ad2d92326f12a263c0ebd022c48d4e64
Saeed Aldosary <saldosary@scai.sa>
2024-10-06 07:17:26 +0300

    Push router

--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const NewUsersPage = () => {
-  return <div>NewUsersPage</div>;
+  const router = useRouter();
+  return (
+    <div>
+      <button className="btn btn-primary" onClick={() => router.push("/users")}>
+        Create
+      </button>
+      NewUsersPage
+    </div>
+  );
 };
 
 export default NewUsersPage;
