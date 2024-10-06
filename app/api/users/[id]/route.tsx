82e8558ee9e80add4f71f006fc95ed8d9ee85141
Saeed Aldosary <saldosary@scai.sa>
2024-10-06 15:12:29 +0300

    Using zod to add validations

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import schema from "../schema";
 
 interface Props {
   params: {
@@ -18,11 +19,14 @@ export function GET(request: NextRequest, { params: { id } }: Props) {
 
 export async function PUT(request: NextRequest, { params: { id } }: Props) {
   const body = await request.json();
+  const validation = schema.safeParse(body);
+
+  if (!validation.success) {
+    return NextResponse.json(validation.error.errors, { status: 400 });
+  }
+
   if (id > 10) {
     return NextResponse.json({ error: "User is not found" }, { status: 404 });
-  }
-  if (!body.name) {
-    return NextResponse.json({ error: "Name is required" }, { status: 400 });
   }
 
   return NextResponse.json({ id: 1, name: body.name });
--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import schema from "./schema";
 
 // If the request removed the data will be cached
 export function GET(request: NextRequest) {
@@ -10,8 +11,9 @@ export function GET(request: NextRequest) {
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  if (!body.name) {
-    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  const validation = schema.safeParse(body);
+  if (!validation.success) {
+    return NextResponse.json(validation.error.errors, { status: 400 });
   }
   return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
 }
