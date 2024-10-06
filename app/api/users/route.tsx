d2b27833bfd9d8e73c9f6ed2ac28a8d8455587d5
Saeed Aldosary <saldosary@scai.sa>
2024-10-06 14:31:19 +0300

    Create post API

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,9 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
 
-// If the request remove the data will be cached
+// If the request removed the data will be cached
 export function GET(request: NextRequest) {
   return NextResponse.json([
     { id: 1, name: "Saeed" },
     { id: 2, name: "Mohammed" }
   ]);
+}
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  if (!body.name) {
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  }
+  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
 }
