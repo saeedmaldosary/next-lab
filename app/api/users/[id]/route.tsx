64f9a76dbb5ec3f0970d72db2b1513e35f534cd8
Saeed Aldosary <saldosary@scai.sa>
2024-10-08 07:29:28 +0300

    Get single user

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,20 +1,27 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "../schema";
+import prisma from "../../../../prisma/client";
 
 interface Props {
   params: {
     // We can have multi parm routes currently we only have id
-    id: number;
+    id: string;
   };
 }
 
 // If the request remove the data will be cached
-export function GET(request: NextRequest, { params: { id } }: Props) {
-  if (id > 10) {
+export async function GET(request: NextRequest, { params: { id } }: Props) {
+  const userId = parseInt(id, 10);
+
+  const user = await prisma.user.findUnique({
+    where: { id: userId }
+  });
+
+  if (!user) {
     return NextResponse.json({ error: "User not found" }, { status: 404 });
   }
 
-  return NextResponse.json({ id: id, name: "Mosh" });
+  return NextResponse.json(user);
 }
 
 export async function PUT(request: NextRequest, { params: { id } }: Props) {
@@ -25,7 +32,7 @@ export async function PUT(request: NextRequest, { params: { id } }: Props) {
     return NextResponse.json(validation.error.errors, { status: 400 });
   }
 
-  if (id > 10) {
+  if (id > "10") {
     return NextResponse.json({ error: "User is not found" }, { status: 404 });
   }
 
@@ -33,7 +40,7 @@ export async function PUT(request: NextRequest, { params: { id } }: Props) {
 }
 
 export async function DELETE(request: NextRequest, { params: { id } }: Props) {
-  if (id > 10) {
+  if (id > "10") {
     return NextResponse.json({ error: "User is not found" }, { status: 404 });
   }
   if (!id) {
