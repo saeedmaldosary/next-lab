f0915fdd0ef176e1f8731e8cdbd0e85c8d019253
Saeed Aldosary <saldosary@scai.sa>
2024-10-08 09:35:11 +0300

    Delete and edit user in the db

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -25,6 +25,7 @@ export async function GET(request: NextRequest, { params: { id } }: Props) {
 }
 
 export async function PUT(request: NextRequest, { params: { id } }: Props) {
+  const userId = parseInt(id, 10);
   const body = await request.json();
   const validation = schema.safeParse(body);
 
@@ -32,20 +33,42 @@ export async function PUT(request: NextRequest, { params: { id } }: Props) {
     return NextResponse.json(validation.error.errors, { status: 400 });
   }
 
-  if (id > "10") {
-    return NextResponse.json({ error: "User is not found" }, { status: 404 });
+  const user = await prisma.user.findUnique({
+    where: { id: userId }
+  });
+
+  if (!user) {
+    return NextResponse.json({ error: "User not exist" }, { status: 400 });
   }
 
-  return NextResponse.json({ id: 1, name: body.name });
+  const updatedUser = await prisma.user.update({
+    where: { id: userId },
+    data: {
+      name: body.name,
+      email: body.email
+    }
+  });
+
+  return NextResponse.json(updatedUser);
 }
 
 export async function DELETE(request: NextRequest, { params: { id } }: Props) {
-  if (id > "10") {
-    return NextResponse.json({ error: "User is not found" }, { status: 404 });
-  }
+  const userId = parseInt(id, 10);
   if (!id) {
     return NextResponse.json({ error: "ID is required" }, { status: 400 });
   }
 
-  return NextResponse.json({});
+  const user = await prisma.user.findUnique({
+    where: { id: userId }
+  });
+
+  if (!user) {
+    return NextResponse.json({ error: "User not exist" }, { status: 400 });
+  }
+
+  const deletedUser = await prisma.user.delete({
+    where: { id: userId }
+  });
+
+  return NextResponse.json(deletedUser);
 }
