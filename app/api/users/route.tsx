824c620c936d2638899d0e1b3ea8b4fd541cfb59
Saeed Aldosary <saldosary@scai.sa>
2024-10-08 07:20:40 +0300

    Setup prisma client and get all users using it

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,12 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import prisma from "../../../prisma/client";
 
 // If the request removed the data will be cached
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-    { id: 1, name: "Saeed" },
-    { id: 2, name: "Mohammed" }
-  ]);
+export async function GET(request: NextRequest) {
+  const users = await prisma.user.findMany();
+  return NextResponse.json(users);
 }
 
 export async function POST(request: NextRequest) {
