1194b48a02efd6eeda9ce1ede0a992589907f6d2
Saeed Aldosary <saldosary@scai.sa>
2024-10-13 09:40:41 +0300

    Use Prisma adapter and implement middleware

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,20 @@
 // This init next auth file
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "@/prisma/client";
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!
     })
-  ]
+  ],
+  session: {
+    strategy: "jwt"
+  }
 };
 
 const handler = NextAuth(authOptions);
